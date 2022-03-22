df919d21d1d39b1fc231ad4f9568706a918a2e65
Trần Hạ Lan <halan4444b@gmail.com>
2022-03-22 15:02:22 +0700

    long6

---
+++ BT1_HINHOC3.java
@@ -0,0 +1,9 @@
+public class BT1_HINHOC3 {
+    public static void main(String[] args) {
+        for(int i=0; i<5; i++){
+            for(int j=5; j>i; j--) {
+                System.out.print("*");
+            } System.out.println("");
+        }
+    }
+}
---
+++ BT2_20SNT.java
@@ -0,0 +1,23 @@
+
+public class BT2_20SNT {
+        public static void main(String[] args) {
+            int count = 0;
+            int number = 2;
+            int demUoc = 0;
+            while (count<20){
+                for (int i = 2; i<= Math.sqrt(number);i++){
+                    if (number % i == 0){
+                        demUoc++;
+                        break;
+                    }
+                }
+                if (demUoc == 0){
+                    count += 1;
+                    System.out.print(count + ". ");
+                    System.out.println(number);
+                }
+                demUoc = 0;
+                number += 1;
+            }
+        }
+}
